5e6fd380dcf236cf6065f54429da71def9cd1d4e
John Doe <johndoe@example.com>
2026-06-11 13:47:12 +0200

    removed one time installer, increased allowance for voltage variantion

--- PikaPV-installer/check-source.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0check-source.ps1" %*
-exit /b %ERRORLEVEL%
-
--- PikaPV-installer/run-dev.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0run-dev.ps1" %*
-exit /b %ERRORLEVEL%
-
